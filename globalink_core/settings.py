01149bef0c21d1853de5b8de7dd1c0d17ece9f4f
Yusuf-Babagana <yusufmuhdkk@gmail.com>
2026-02-06 04:52:53 -0800

    Migrated to Mysql

--- globalink_core/settings.py
+++ globalink_core/settings.py
@@ -88,10 +88,14 @@ WSGI_APPLICATION = 'globalink_core.wsgi.application'
 # 7. Database
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT', '3306'),
         'OPTIONS': {
-            'timeout': 20,
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
         }
     }
 }
