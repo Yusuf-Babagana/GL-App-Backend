6875dc9b799f7a294d7a2b71f8dae64b9a920a34
Yusuf-Babagana <yusufmuhdkk@gmail.com>
2026-02-11 03:06:11 -0800

    Chat functionality

--- chat/serializers.py
+++ chat/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import Conversation, Message
 from django.contrib.auth import get_user_model
+from django.utils import timezone
 
 User = get_user_model()
 
@@ -22,10 +23,11 @@ class ConversationSerializer(serializers.ModelSerializer):
     other_user_id = serializers.SerializerMethodField() # Added for navigation
     last_message = serializers.SerializerMethodField()
     unread_count = serializers.SerializerMethodField() # Added for badges
+    other_user_status = serializers.SerializerMethodField() # Added for online status
 
     class Meta:
         model = Conversation
-        fields = ['id', 'other_user_name', 'other_user_id', 'last_message', 'unread_count', 'updated_at']
+        fields = ['id', 'other_user_name', 'other_user_id', 'other_user_status', 'last_message', 'unread_count', 'updated_at']
 
     def get_other_user_name(self, obj):
         user = self.context['request'].user
@@ -46,6 +48,20 @@ class ConversationSerializer(serializers.ModelSerializer):
         return last.text if last else "New Conversation"
 
     def get_unread_count(self, obj):
+        user = self.context['request'].user
+        # Count messages not sent by me that are still 'is_read=False'
+        return obj.messages.filter(is_read=False).exclude(sender=user).count()
+
+    def get_other_user_status(self, obj):
+        user = self.context['request'].user
+        other = obj.participants.exclude(id=user.id).first()
+        if other:
+            # If the user logged in within the last 5 minutes, consider them "Active Now"
+            if other.last_login and (timezone.now() - other.last_login).total_seconds() < 300:
+                return "Active now"
+            elif other.last_login:
+                return f"Last seen {other.last_login.strftime('%H:%M')}"
+        return "Offline"
         user = self.context['request'].user
         # Count messages not sent by me that are still 'is_read=False'
         return obj.messages.filter(is_read=False).exclude(sender=user).count()
