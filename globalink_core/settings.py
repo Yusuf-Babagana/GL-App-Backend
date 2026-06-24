a76e0004673f5a40c75c90ab23127dbd5c488a9b
Yusuf-Babagana <yusufmuhdkk@gmail.com>
2026-06-24 07:07:00 -0700

    fix: remove SessionAuthentication from DRF defaults to fix 403 on registration

--- globalink_core/settings.py
+++ globalink_core/settings.py
@@ -169,7 +169,6 @@ FRONTEND_URL = env('FRONTEND_URL', default='http://localhost:3000')
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
